c6c5726825584183454be9200bbfaa884d7f8907
qqgameover <kasper35@live.no>
2021-10-02 16:27:42 +0200

    Initial commit

---
+++ eulerproblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eulerproblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
